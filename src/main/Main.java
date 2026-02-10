3f01ce5b61b4e84e9cbac96b8c7e02c9d5e23385
Pavan Kalyan <daroorpavankalyan@gmail.com>
2026-02-10 19:57:12 +0530

    Initial student program

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.id = 1;
+        s.name = "Kalyan";
+
+        s.id =2;
+        s.name ="pavan";
+
+        s.display();
+    }
+}
---
+++ src/main/Student.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Student {
+    int id;
+    String name;
+
+    void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+    }
+}
